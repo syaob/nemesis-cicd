9f6e0a079a179da7bc9bd66a0029514a65bd1aac
saikou balde <syaob@yahoo.fr>
2025-06-16 14:55:06 +0200

    server maj dans branch dev

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 app.get("/", (req, res) => {
-  res.send("Hello World from cloud campus nemesis v2!");
+  res.send("Hello World from cloud campus nemesis v2! branch dev modification");
 });
+
+app.get("/nemesis", (req, res) => {
+  res.send("route nemesis");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
