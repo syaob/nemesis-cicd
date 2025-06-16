cd3bd38fbc94e913eb139d9fb9c2e34f6a35c4a4
saikou balde <syaob@yahoo.fr>
2025-06-16 14:24:13 +0200

    création de la première route de l'api '/'

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-//mise en place code express API 
+//mise en place code express API
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World from cloud campus nemesis v2!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
